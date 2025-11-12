044147a17df73b9d3f7cfccc25e6add94aa34272
izumi-shoj <izumi6204@gmail.com>
2025-11-12 21:43:09 +0900

    UC01 ログイン/ログアウト機能修正（JWT標準化・不要URL削除）

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,9 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import LoginView, LogoutView
 
 urlpatterns = [
-    # カスタムログインAPI
-    path("auth/login/custom/", LoginView.as_view(), name="custom_login"),
-
+    
     # JWTログイン関連
     path("auth/login/", TokenObtainPairView.as_view(), name="login"),  # JWTログイン
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),  # トークン更新
