8a548e714b427ef73fd979043e18ef41ca22126e
oomori <nagumorigalaxy@gmail.com>
2025-11-15 16:41:09 +0900

    11/15 16:40

--- file/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ListFileView.as_view(), name='list_file'),
-    path('<uuid:pk>/', views.DownloadFileView.as_view(), name='download_file'),
-    path('create/', views.UploadFileView.as_view(), name='uploard_file'),
-    path('<uuid:pk>/delete/', views.DeleteFileView.as_view(), name='delete_file'),
-]
