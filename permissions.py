5a2a490f0c412b6a1b09eaa1631daa485baddad9
oomori <nagumorigalaxy@gmail.com>
2025-12-08 14:27:34 +0900

    2025-12-08 14:30 permissons修正

--- permissions.py
+++ permissions.py
@@ -22,19 +22,3 @@ class IsAdminOrAuthenticatedReadOnly(BasePermission):
             return True
 
         return False
-
-
-
-# 未ログイン者でも見れる場合(今のところ未実装)
-class IsAdminOrReadOnly(BasePermission):
-
-    def has_permission(self, request, view):
-        # 管理者 (admin) は全て許可
-        if request.user.role == 'admin':
-            return True
-
-        # それ以外も読み取り操作 (GET, HEAD, OPTIONS) なら許可
-        if request.method in SAFE_METHODS:
-            return True
-
-        return False
