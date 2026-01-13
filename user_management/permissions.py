53172e3cd01bab5f52a57ac0a0bc5682bbf18248
izumi-shoj <izumi6204@gmail.com>
2026-01-13 14:09:10 +0900

    修正版permissions commit忘れ分

--- user_management/permissions.py
+++ user_management/permissions.py
@@ -1,31 +1,15 @@
 from rest_framework.permissions import BasePermission
 
 class IsUserAdmin(BasePermission):
-        """
-        UC08 ユーザー管理専用 Permission
+    """
+    管理者は全操作許可
+    一般ユーザーは読み取りのみ許可
+    """
 
-    一般ユーザー:
-    list (一覧)
-    retrieve (詳細取得)
-        のみ許可
+    def has_permission(self, request, view):
+        # 読み取り系アクションは全ユーザー許可
+        if view.action in ["list", "retrieve"]:
+            return True
 
-    管理者 (is_staff=True):
-    create
-    update
-    partial_update
-    destroy
-    set_active_status
-    bulk_generate
-    bulk_upload
-        を含むすべての操作を許可
-
-        ※ HTTPメソッドではなく、ViewSet の action で判定するのが重要
-        """
-
-        def has_permission(self, request, view):
-            # 読み取り系アクションは全ユーザー許可
-            if view.action in ["list", "retrieve"]:
-                return True
-
-            # それ以外（作成・変更・削除・一括登録等）は管理者のみ
-            return request.user and request.user.is_staff
+        # それ以外（作成・変更・削除・一括登録等）は管理者のみ
+        return request.user and request.user.is_staff
